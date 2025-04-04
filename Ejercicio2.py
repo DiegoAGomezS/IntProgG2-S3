8863a1df5d6433d66d31373303b601c198383794
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-04-04 07:58:21 -0600

    Organización de información

--- Ejercicio2.py
+++
@@ -1,8 +0,0 @@
-print("Por favor brinde la base y la altura del rectángulo")
-base = float(input("Base"))
-altura = float(input("Altura"))
-area = base * altura
-perimetro = (2*base)+(2*altura)
-
-print (f'El área es igual a: {area:.2f}')
-print (f'El perimetro es igual a: {perimetro:.2f}')
---
+++ Sección_3/CordobasDolaresEuros.py
@@ -0,0 +1,9 @@
+#Convertir cordobas a otras monedas
+cordobas = int(input("Ingrese la cantidad de cordobas que tiene: "))
+
+#Cálculos
+euros = cordobas / 43.08
+print(f'Su cantidad en euros es: {euros:.2f}')
+
+dolares = cordobas / 36.75
+print(f'Su cantidad en dólares es: {dolares:.2f}')
